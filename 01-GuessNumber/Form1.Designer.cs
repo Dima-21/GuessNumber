d3fff8c3a21539c56db0bbdd8848f9d110c35f71
Dimon <Dimon@DESKTOP-RMP3SSA>
2018-05-23 23:25:23 +0300

    1.0

--- 01-GuessNumber/Form1.Designer.cs
+++ 01-GuessNumber/Form1.Designer.cs
@@ -29,11 +29,259 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LHard = new System.Windows.Forms.RadioButton();
+            this.LAverage = new System.Windows.Forms.RadioButton();
+            this.LEasy = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHistory = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textTime = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LHard);
+            this.groupBox1.Controls.Add(this.LAverage);
+            this.groupBox1.Controls.Add(this.LEasy);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Уровень";
+            // 
+            // LHard
+            // 
+            this.LHard.AutoSize = true;
+            this.LHard.Location = new System.Drawing.Point(6, 65);
+            this.LHard.Name = "LHard";
+            this.LHard.Size = new System.Drawing.Size(76, 17);
+            this.LHard.TabIndex = 3;
+            this.LHard.TabStop = true;
+            this.LHard.Text = "Сложно(7)";
+            this.LHard.UseVisualStyleBackColor = true;
+            // 
+            // LAverage
+            // 
+            this.LAverage.AutoSize = true;
+            this.LAverage.Location = new System.Drawing.Point(6, 42);
+            this.LAverage.Name = "LAverage";
+            this.LAverage.Size = new System.Drawing.Size(95, 17);
+            this.LAverage.TabIndex = 2;
+            this.LAverage.TabStop = true;
+            this.LAverage.Text = "Нормально(5)";
+            this.LAverage.UseVisualStyleBackColor = true;
+            // 
+            // LEasy
+            // 
+            this.LEasy.AutoSize = true;
+            this.LEasy.Location = new System.Drawing.Point(6, 19);
+            this.LEasy.Name = "LEasy";
+            this.LEasy.Size = new System.Drawing.Size(68, 17);
+            this.LEasy.TabIndex = 1;
+            this.LEasy.TabStop = true;
+            this.LEasy.Text = "Легко(3)";
+            this.LEasy.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "История";
+            // 
+            // textHistory
+            // 
+            this.textHistory.Location = new System.Drawing.Point(13, 143);
+            this.textHistory.Multiline = true;
+            this.textHistory.Name = "textHistory";
+            this.textHistory.ReadOnly = true;
+            this.textHistory.Size = new System.Drawing.Size(218, 202);
+            this.textHistory.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textNum);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(131, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(100, 94);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Введите число";
+            // 
+            // textNum
+            // 
+            this.textNum.Location = new System.Drawing.Point(7, 20);
+            this.textNum.Name = "textNum";
+            this.textNum.Size = new System.Drawing.Size(87, 20);
+            this.textNum.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(7, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Число:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Совпало цифр:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 82);
+            this.label4.MaximumSize = new System.Drawing.Size(100, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Позиционно совпало цифр:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(401, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(401, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(401, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(250, 248);
+            this.label5.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 50);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Количество попыток:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(422, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 55);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textTime
+            // 
+            this.textTime.AutoSize = true;
+            this.textTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textTime.Location = new System.Drawing.Point(248, 180);
+            this.textTime.Name = "textTime";
+            this.textTime.Size = new System.Drawing.Size(242, 37);
+            this.textTime.TabIndex = 12;
+            this.textTime.Text = "00 : 00 : 00 : 00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(511, 357);
+            this.Controls.Add(this.textTime);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textHistory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton LHard;
+        private System.Windows.Forms.RadioButton LAverage;
+        private System.Windows.Forms.RadioButton LEasy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHistory;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label textTime;
     }
 }
 
--- 01-GuessNumber/Form1.cs
+++ 01-GuessNumber/Form1.cs
@@ -12,9 +12,40 @@ namespace _01_GuessNumber
 {
     public partial class Form1 : Form
     {
+        DateTime timer;
+        Random rand = new Random();
+        long num;
+        bool isStart = false;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        private void CreateNumber()
+        {
+            if(LEasy.Checked)
+                num = rand.Next(100, 1000);
+            else if(LAverage.Checked)
+                num = rand.Next(10000, 100000);
+            else if(LHard.Checked)
+                num = rand.Next(1000000, 10000000);
+            isStart = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!isStart)
+                CreateNumber();
+            timer1.Start();
+            timer = DateTime.Now;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks - timer.Ticks;
+            DateTime stopWatch = new DateTime();
+            stopWatch = stopWatch.AddTicks(tick);
+
+            textTime.Text = String.Format("{0:HH : mm : ss : ff}", stopWatch);
         }
     }
 }
